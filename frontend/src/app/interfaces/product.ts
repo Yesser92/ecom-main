591fd17c9e0a95511c65a28f3200fb513c0b91c0
Montassar <montassar.rebhi@fst.utm.tn>
2023-05-12 19:23:21 +0100

    product-comp

---
+++ frontend/src/app/interfaces/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  product_id: number;
+  product_name: string;
+  description: string;
+  price: string;
+  stock: number;
+  image: string;
+  created_at: Date;
+  updated_at: Date;
+}
