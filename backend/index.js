5d0a9dcb2cef14fcb21b8a1cdb927e57e35068ef
yesser khammessi <khammessiyasser@gmail.com>
2023-05-18 20:49:16 +0200

    payment

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,6 @@
-
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const users = require('./Routes/user.js');
-const products = require('./Routes/product.js')
-const cart= require('./Routes/cart.js')
-const category= require('./Routes/category.js')
-const sequelize = require('./ORM/index.js');
-const order = require('./Routes/order.js')
+const stripe = require('stripe')('sk_test_51MwW2YKqLFfv80mnKqfe0Wb52NPtxT6tjyQKpL0PGC4ubBEpj2gjTsTptbPozhA7TDKbpACQhlxa1c62TYl4kQt800y5j81RXA'); // Replace with your Stripe secret key
 
 const app = express();
 
@@ -14,21 +8,45 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Use the routes
-app.use("/api/users", users);
-app.use("/api/products", products);
-app.use("/api/cart", cart);
-app.use('/api/category',category)
-app.use('/api/orders',order)
+// Endpoint to process payments
+app.post('/api/payment', async (req, res) => {
+  const { amount, token } = req.body;
+  stripe.paymentMethods.create(
+    {
+      type: 'card',
+      card: {
+        token: token.card.token,
+      },
+    },
+    async function(err, paymentMethod) {
+      if (err) {
+        console.log(err);
+        // Handle error
+      } else {
+        console.log(paymentMethod);
 
-// Sync with the database and start the server
-sequelize.sync()
-  .then(() => {
-    const port = 3000;
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+        try {
+          const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: 'usd',
+            payment_method_types: ['card'],
+            payment_method: paymentMethod.id,
+            confirm: true
+          });
+          res.status(200).json({ paymentIntent });
+        } catch (error) {
+          console.log(error);
+          res.status(500).json({ error: 'Error creating payment intent' });
+        }
+
+        // PaymentMethod created successfully
+      }
+    }
+  );
+ 
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomersComponent } from './components/customers/customers.component';
@@ -26,7 +25,6 @@ import { StoreComponent } from './pages/store/store.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
 import { ManageProductsComponent } from './components/manage-products/manage-products.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,6 +49,7 @@ import { ManageProductsComponent } from './components/manage-products/manage-pro
     AddProductComponent,
     ManageProductsComponent,
     ProductItemComponent,
+    
     
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
